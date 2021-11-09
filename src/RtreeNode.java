43129d1e742801adc329eab2999aa7cb01e5bb5c
ShirahaneSuoh03 <91019145+ShirahaneSuoh03@users.noreply.github.com>
2021-11-09 17:32:22 +0800

    Add files via upload
    Search Part

---
+++ src/RtreeNode.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class RtreeNode {
+    String pointer;
+    Double xupper, xlower, yupper, ylower;
+    public RtreeNode(String s){
+        this.pointer=s;
+    }
+
+}
---
+++ src/knnGrid.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+/**
+ * 
+ */
+
+/**
+ * @author huangjunxiang
+ * class of the knn grid that stores various of information and relevant function
+ */
+public class knnGrid {
+	private static HashMap<String, Set<SpatialRelation>> spatialSets = new HashMap<>();
+	/**
+	 * @param s
+	 * @param sr
+	 */
+	public static void addSpatialRelation(String s, SpatialRelation sr) {
+		if(spatialSets.containsKey(s)) {
+			spatialSets.get(s).add(sr);
+		}
+		else {
+			spatialSets.put(s, new HashSet<SpatialRelation>());
+			spatialSets.get(s).add(sr);
+		}
+	}
+	/**
+	 * print the size
+	 */
+	public static void showValue() {
+		System.out.println(spatialSets.size());
+	}
+	/**
+	 * @param s
+	 * @return
+	 */
+	public static Set<SpatialRelation> getSpatialRelation(String s) {
+		Set<SpatialRelation> sr=spatialSets.get(s);
+		return sr;
+	}
+	/**
+	 * @param s
+	 * @return whether contain key s
+	 */
+	public static boolean containKeys(String s) {
+		return spatialSets.containsKey(s);
+	}
+	public static void reset() {
+		spatialSets=new HashMap<String, Set<SpatialRelation>>();
+	}
+}
